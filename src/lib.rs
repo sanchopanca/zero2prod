369d43c9c57445efe1388f3d293938e6bfb63651
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2024-03-12 18:31:11 +0100

    Add a dummy form handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,34 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    dev::Server,
+    web::{self, Form},
+    App, HttpResponse, HttpServer, Responder,
+};
+
+#[allow(dead_code)]
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+async fn subscribe(_form: Form<FormData>) -> impl Responder {
+    HttpResponse::Ok()
+}
+
 pub fn run(listener: TcpListener) -> std::io::Result<Server> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
