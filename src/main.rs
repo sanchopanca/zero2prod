ad7d5bde3a59415d95460b7003c9873bee2de583
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2024-03-12 15:30:17 +0100

    Spawn the app on a random port in tests
    This will allow running the app and the tests at the same time

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::net::TcpListener;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    zero2prod::run()?.await
+    zero2prod::run(TcpListener::bind("127.0.0.1:8000")?)?.await
 }
